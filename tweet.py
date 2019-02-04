90e522b1c3fef39e5bcf6252b127f96f2dfba8e5
Dorsa Ghoreishi <44623334+Dorigh@users.noreply.github.com>
2019-02-04 02:55:33 -0600

    python code for twitter API

---
+++ tweet.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import json
+from twython import Twython
+
+states = pd.read_csv('/Users/dori/Documents/Challenge/States.csv')
+states.head()
+
+credentials = {}  
+credentials['APP_KEY'] = 'DocHIzPvJzR9SHRlHCzAnN3Nj' 
+credentials['APP_SECRET'] =  'XENnUXjHFFvjbOOCl9JWSGkOdNUjYNXwxcsCusCGdjrxCtgzqX' 
+credentials['ACCESS_TOKEN'] = '4114800372-XjiEQLWVA4cVxdWad5EUgSYGwdj5qt0oCskyN5q' 
+credentials['ACCESS_SECRET'] = '892y9FVWYu70NptCY8wdZI05vvCC1bOOek4GjStXf24d'
+
+with open("twitter_credentials.json", "w") as file:  
+    json.dump(credentials, file)
+    
+
+with open("twitter_credentials.json", "r") as file:  
+    creds = json.load(file)
+
+
+python_tweets = Twython(creds['APP_KEY'], creds['APP_SECRET'])
+
+geo = states['Geo']
+sta = states['State']
+par = states['Political Party']
+
+dict_ = {'user': [], 'date': [], 'text': [], 'id':[], 'favorite_count': [], 'retweet_num': [], 'state': [], 'party': []} 
+for i in range(0,len(geo)):
+    query = {'q': '#globalwarming', 
+             'count': '200',
+             'lang': 'en',
+             'until': '2019-02-03',
+             'geocode': geo[i]}
+    
+    for status in python_tweets.search(**query)['statuses']:  
+        dict_['user'].append(status['user']['screen_name'])
+        dict_['date'].append(status['created_at'])
+        dict_['text'].append(status['text'])
+        dict_['id'].append(status['id'])
+        dict_['favorite_count'].append(status['favorite_count'])
+        dict_['retweet_num'].append(status['retweet_count'])
+        dict_['state'].append(sta[i])
+        dict_['party'].append(par[i])
+        
+        
+df = pd.DataFrame(dict_)
+
+writer = pd.ExcelWriter('pandas_simple2.xlsx', engine = 'xlsxwriter')
+df.to_excel(writer, sheet_name = 'Sheet1')
+writer.save()
+
